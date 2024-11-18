cd5463911642f173e7457d7ccab449aff238bebb
Евгения <fullsucsess@gmail.com>
2024-11-19 00:10:02 +0500

    Task 2

--- shopping/games/models.py
+++ shopping/games/models.py
@@ -1,21 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Buyer(models.Model):
     name = models.CharField(max_length=20)
-    balance = models.DecimalField(max_digits=5,decimal_places=3)
+    balance = models.DecimalField(max_digits=6, decimal_places=3)
     age = models.IntegerField()
 
     def __str__(self):
         return self.name
 
+
 class Game(models.Model):
     title = models.CharField(max_length=20)
-    cost = models.DecimalField(max_digits=5,decimal_places=3)
-    size = models.DecimalField(max_digits=5,decimal_places=3)
+    cost = models.DecimalField(max_digits=5, decimal_places=1)
+    size = models.DecimalField(max_digits=5, decimal_places=1)
     description = models.TextField()
     age_limited = models.BooleanField(default=False)
-    buyer = models.ManyToManyField(Buyer,related_name='games')
+    buyer = models.ManyToManyField(Buyer, related_title='games')
+
 
     def __str__(self):
-        return self.name
+        return self.title
