b38b0a26ccbb7de18ac93ec18114f0ebef6da4e6
Евгения <fullsucsess@gmail.com>
2024-11-21 22:08:38 +0500

    Task 4

---
+++ shopping/games/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+class UserRegister(forms.Form):
+    username = forms.CharField(max_length=30, label='Введите логин')
+    password = forms.CharField(min_length=8, label='Введите пароль',widget=forms.PasswordInput)
+    repeat_password = forms.CharField(min_length=8, label='Повторите пароль',widget=forms.PasswordInput)
+    age = forms.IntegerField(min_value=3,max_value=100, label='Введите свой возраст')
